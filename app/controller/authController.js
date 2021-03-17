f513ec8dfbc515a82ebbf4210df0a6bf7c8d5ede
Hasobi Ro'id Radityo <13960580+hasobi@users.noreply.github.com>
2021-03-17 12:04:09 +0700

    added authController and authJwt

---
+++ app/controller/authController.js
@@ -0,0 +1,50 @@
+const { baseResponse } = require("../utils/helpers/baseResponse");
+const { user } = require("./../db/models");
+const bcrypt = require("bcrypt");
+const { generateJWT } = require("../utils/middleware/authJwt");
+
+class AuthController {
+  static async login(req, res, next) {
+    try {
+      const data = await user.findOne({
+        where: {
+          email: req.body.email,
+        },
+      });
+      //   console.log(data)
+      if (data) {
+        const check = bcrypt.compareSync(req.body.password, data.password);
+        console.log(check);
+        if (check) {
+          const payload = {
+            id: data.id,
+            fullname:data.fullname,
+            username: data.username,
+            email: data.email,
+            photo:data.photo,
+            role: data.role,
+          };
+          payload.token = generateJWT(payload);
+          return baseResponse({
+            message: "logged in",
+            data: { ...payload },
+          })(res, 200);
+        } else {
+          return baseResponse({
+            success: false,
+            message: "wrong email or password",
+          })(res, 200);
+        }
+      } else {
+        return baseResponse({ success: false, message: "user doesn't exist" })(
+          res,
+          401
+        );
+      }
+    } catch (error) {
+      res.status(500);
+      next(error);
+    }
+  }
+}
+module.exports = AuthController;
