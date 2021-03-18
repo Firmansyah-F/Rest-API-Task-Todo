6df46139f10dab327c40c4e28973d6643dd9a6d7
Hasobi Ro'id Radityo <13960580+hasobi@users.noreply.github.com>
2021-03-18 18:26:16 +0700

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
-const port = 80;
+const port = 3000;
 const cors = require("cors");
 const morgan = require("morgan");
 const { errorHandler } = require("./app/utils/middleware/errorHandling");
