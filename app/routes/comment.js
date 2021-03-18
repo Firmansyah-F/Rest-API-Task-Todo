446418ba7e16ba731b026295675efba101f78e6a
F <fikrifirmansyah096@gmail.com>
2021-03-18 13:46:40 +0700

    update

--- app/routes/comment.js
+++ app/routes/comment.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const CommentController = require("./../controller/commentControllers");
+const { verifyJwt, permit } = require("./../utils/middleware/authJwt");
 
-router
-  .route('/')
-  .post(function(req, res){CommentController.createComment})
-  .get(CommentController.getAll);
 
 router
   .route("/:id")
-  .get(CommentController.getById)
-  .put(CommentController.update)
-  .delete(CommentController.delete)
+  .post([verifyJwt, permit("admin", "user", "supervisor")],CommentController.create)
+  .get([verifyJwt, permit("admin","supervisor")],CommentController.getAll)
+  // .put([verifyJwt, permit("admin", "user", "supervisor")],CommentController.update)
+  .delete([verifyJwt, permit("admin", "supervisor")], CommentController.delete)
+
 
 module.exports = {router}
+
