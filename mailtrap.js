cfe9cf29d72e7fe43a4b475c3d8743e25440d4a8
Hasobi Ro'id Radityo <13960580+hasobi@users.noreply.github.com>
2021-03-18 18:48:19 +0700

    upadate

---
+++ mailtrap.js
@@ -0,0 +1,8 @@
+var transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "211c0324ba9115",
+      pass: "1956c79ffb0ed1"
+    }
+  });
---
+++ nodemailer.js
@@ -0,0 +1,48 @@
+"use strict";
+const nodemailer = require("nodemailer");
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+//   let transporter = nodemailer.createTransport({
+    // host: "smtp.ethereal.email",
+    // port: 587,
+    // secure: false, // true for 465, false for other ports
+    // auth: {
+    //   user: testAccount.user, // generated ethereal user
+    //   pass: testAccount.pass, // generated ethereal password
+    // },
+//   });
+
+let transporter = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "211c0324ba9115",
+      pass: "1956c79ffb0ed1"
+    },
+  });
+
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <roid.radityo11@gmail.com>', // sender address
+    to: "roid.siboy@yahoo.co.id", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+main().catch(console.error);
