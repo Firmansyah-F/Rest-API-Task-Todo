e4fc6b1a00dd5f4e16df91618be7448d9b6c185d
Teguh Setiawan <homepage408@gmail.com>
2021-03-18 13:45:50 +0700

    Task Todo

--- app/routes/task.js
+++ app/routes/task.js
@@ -2,13 +2,12 @@ const express = require("express");
 const router = express.Router();
 const TaskController = require("./../controller/taskController");
 const { verifyJwt, permit } = require("./../utils/middleware/authJwt");
+// const { updateStatus } = require("./../utils/helpers/validationSchema");
+const { validateStatus } = require("./../utils/middleware/validate");
 
 router
   .route("/")
-  .post(
-    [verifyJwt, permit("admin", "user", "supervisor")],
-    TaskController.createTask
-  )
+  .post([verifyJwt, permit("admin", "supervisor")], TaskController.createTask)
   .get(
     [verifyJwt, permit("admin", "user", "supervisor")],
     TaskController.getAllTask
@@ -17,14 +16,18 @@ router
 // router.route("/role").get(verifyJwt, TaskController.getIdTaskByRoleAssigne);
 router
   .route("/:id")
-  .put(
-    [verifyJwt, permit("admin", "user", "supervisor")],
-    TaskController.updateTask
-  )
   .delete([verifyJwt, permit("admin", "supervisor")], TaskController.deleteTask)
   .get(
     [verifyJwt, permit("admin", "user", "supervisor")],
-    TaskController.getAllById
+    TaskController.getByIdTask
+  )
+  .put([verifyJwt, permit("admin", "supervisor")], TaskController.updateTask);
+
+router
+  .route("/updateStatus/:id")
+  .patch(
+    [verifyJwt, permit("user", "supervisor", "admin"), validateStatus],
+    TaskController.updateTaskStatus
   );
 
 module.exports = { router };
--- app/utils/middleware/validate.js
+++ app/utils/middleware/validate.js
@@ -1,13 +1,37 @@
+// const { string } = require("yup/lib/locale");
+const yup = require("yup")
+
 const validate = (schema) => async (req, res, next) => {
-    {
-      try {
-        await schema.validate(req.body);
-        next();
-      } catch (error) {
-        res.status(400);
-        next(error.message);
-      }
+  {
+    try {
+      await schema.validate(req.body);
+      next();
+    } catch (error) {
+      res.status(400);
+      next(error.message);
     }
-  };
-  
-  module.exports= {validate};
+  }
+};
+
+const validateStatus = async (req, res, next) => {
+  {
+    try {
+      const role = req.user.role
+      console.log(`validate `,req.body.status)
+      if (role === "user") {
+        let schema = yup.string().matches(/(todo|complete|work in progres)/).required()
+        await schema.isValid(req.body.status)
+        next()
+      } else {
+        let schema = yup.string().matches(/(need to review)/).required()
+        await schema.isValid(req.body.status)
+        next()
+      }
+    } catch (error) {
+      res.status(500);
+      next(error);
+    }
+  }
+};
+
+module.exports = { validate , validateStatus};
