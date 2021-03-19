5abb5abf273e8e302a66f4fea12bc39330facacf
Teguh Setiawan <homepage408@gmail.com>
2021-03-19 13:03:18 +0700

    port again

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
-const port = 3000 || process.env.PORT 
+const port = 5000 || process.env.PORT 
 const cors = require("cors");
 const morgan = require("morgan");
 const { errorHandler } = require("./app/utils/middleware/errorHandling");
